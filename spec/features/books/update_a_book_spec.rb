c2a5867c7c164cad5d44010432c7dc7fac1ef8ed
Kelvin Checo <checokelvin@gmail.com>
2018-12-18 02:46:47 -0500

    Made changes to the steps taken to complete the update a book feature

--- spec/features/books/update_a_book_spec.rb
+++ spec/features/books/update_a_book_spec.rb
@@ -2,20 +2,26 @@ require 'rails_helper'
 
 RSpec.feature "Update a book", type: :feature do
   scenario "I am able to successfully change the title of a book" do
-    given_i_want_to_edit_the_title_of_a_book
-    when_i_visit_the_book_i_want_to_edit
+    given_i_have_to_edit_the_title_of_a_book
+    and_i_see_the_book_i_have_to_edit
+    when_i_click_on_the_book_i_want_to_edit
     and_i_change_the_title_of_the_book
     and_i_click_the_update_button
     then_i_should_see_the_book_was_successfully_updated
   end
 
   private
-  def given_i_want_to_edit_the_title_of_a_book
+  def given_i_have_to_edit_the_title_of_a_book
     @book = FactoryBot.create(:book)
   end
 
-  def when_i_visit_the_book_i_want_to_edit
-    visit "/books/#{@book.id}/edit"
+  def and_i_see_the_book_i_have_to_edit
+    visit "/books"
+    page.has_content?("#{@book.title}")
+  end
+
+  def when_i_click_on_the_book_i_want_to_edit
+    find(:xpath, "//a[contains(@href,'/books/#{@book.id}/edit')]").click
   end
 
   def and_i_change_the_title_of_the_book
