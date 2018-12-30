19ae277a693470409a33c42021cfad27a626e86a
Kelvin Checo <checokelvin@gmail.com>
2018-12-30 16:08:21 -0500

    Added feature to create a new project

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,38 @@
+class ProjectsController < ApplicationController
+  # calls view to display list of projects
+  def index
+  end
+
+  # calls view that allows user to add a new projects to list
+  # of completed projects
+  def new
+    @project = Project.new
+  end
+
+  # ingest valid parameters and adds a new project to list
+  def create
+    @project = Project.new(project_params)
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to edit_project_path(@project), 
+                      notice: "Project was successfully added." }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # calls view that allows user to apply view/changes to a specific
+  # project
+  def edit
+  end
+
+  private
+  # specifies valid parameters that may be ingested by the project resource
+  def project_params
+    params.require(:project).permit(:name,
+                                    :date_completed,
+                                    :link)
+  end
+end
