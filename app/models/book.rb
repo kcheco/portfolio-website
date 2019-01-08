da1d7b1401423e5b05a5f9175f6f33e2bd1581c2
Kelvin Checo <checokelvin@gmail.com>
2019-01-08 02:12:35 -0500

    Refactored Book model to include Coverable

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,23 +1,7 @@
 class Book < ApplicationRecord
+  include Coverable
+
   #validation rules
   validates_presence_of   :title, :date_read
   validates_presence_of   :image, message: 'Hey, quit being lazy and add the cover of this book!'
-
-  # relationships
-  has_one :image, as: :coverable, dependent: :destroy
-
-  accepts_nested_attributes_for :image, allow_destroy: true, 
-                                        reject_if: -> (attrs) { attrs['source'].blank? }
-
-  # methods
-  # a more readable method to build_association method
-  def with_no_cover
-    build_image
-    self
-  end
-
-  # checks if association is set
-  def has_image?
-    !image.nil?
-  end
 end
