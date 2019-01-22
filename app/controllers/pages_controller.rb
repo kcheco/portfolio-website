be9568172bb61cb56ffc27af539d1fb3abf4bfda
Kelvin Checo <checokelvin@gmail.com>
2019-01-22 14:33:48 -0500

    Added feature to view books last read on the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
     @projects = Project.latest
+    @books = Book.last_read
   end
 end
--- spec/features/pages/view_home_page_spec.rb
+++ spec/features/pages/view_home_page_spec.rb
@@ -2,21 +2,30 @@ require 'rails_helper'
 
 feature "View home page" do
   background do
-    given_there_are_more_than_three_projects
+    given_i_have_a_list_of_projects
+    given_i_have_a_list_of_books_ive_read
   end
 
   scenario do
     when_i_visit_the_home_page
     then_i_should_see_the_heading_of_each_section
     and_i_should_see_the_three_most_recent_projects
+    and_i_should_see_the_three_most_recent_books_ive_read
   end
 
   private
-  def given_there_are_more_than_three_projects
+  def given_i_have_a_list_of_projects
     @project1 = FactoryBot.create(:project, :with_cover, date_completed: 1.year.ago)
     @project2 = FactoryBot.create(:project, :with_cover, date_completed: 1.month.ago)
     @project3 = FactoryBot.create(:project, :with_cover, date_completed: 1.week.ago)
     @project4 = FactoryBot.create(:project, :with_cover, date_completed: 1.day.ago)
+  end
+
+  def given_i_have_a_list_of_books_ive_read
+    @book1 = FactoryBot.create(:book, :with_cover, date_read: 1.year.ago)
+    @book2 = FactoryBot.create(:book, :with_cover, date_read: 1.month.ago)
+    @book3 = FactoryBot.create(:book, :with_cover, date_read: 1.week.ago)
+    @book4 = FactoryBot.create(:book, :with_cover, date_read: 1.day.ago)
   end
 
   def when_i_visit_the_home_page
@@ -27,6 +36,7 @@ feature "View home page" do
     expect(page).to have_content("Kelvin Checo")
     expect(page).to have_content("About me")
     expect(page).to have_content("Projects")
+    expect(page).to have_content("Books I've Read")
   end
 
   def and_i_should_see_the_three_most_recent_projects
@@ -34,5 +44,12 @@ feature "View home page" do
     expect(page).to have_css("img[src='#{@project3.image.source}']")
     expect(page).to have_css("img[src='#{@project2.image.source}']")
     expect(page).to_not have_css("img[src='#{@project1.image.source}']")
+  end
+
+  def and_i_should_see_the_three_most_recent_books_ive_read
+    expect(page).to have_css("img[src='#{@book4.image.source}']")
+    expect(page).to have_css("img[src='#{@book3.image.source}']")
+    expect(page).to have_css("img[src='#{@book2.image.source}']")
+    expect(page).to_not have_css("img[src='#{@book1.image.source}']")
   end
 end
