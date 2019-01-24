e5f4a8f9021e6566f8085b1ff7840686b5c61733
Kelvin Checo <checokelvin@gmail.com>
2019-01-24 14:13:18 -0500

    Refactored code that displays form errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+    def display_form_errors_for instance
+        html = ''
+        error_messages = ''
+        if instance.errors.any?
+          html << "<div class='alert alert-danger' role='alert'>"
+          html << "<h5 class='alert-heading'>There was an error. Fix the #{ pluralize(instance.errors.count, 'error') } below before attempting to save.</h5>"
+          html << "<ul>"
+          
+          instance.errors.full_messages.each do |msg|
+            error_messages << "<li>#{ msg }</li>"
+          end
+          
+          html << error_messages
+          html << "</ul></div>"
+
+          html.html_safe
+        end
+    end
 end
