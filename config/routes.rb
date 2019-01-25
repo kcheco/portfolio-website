4fc34b6eeb5b4b46dba0c5eb489e145e77d186b1
Kelvin Checo <checokelvin@gmail.com>
2019-01-25 01:02:22 -0500

    Initial configuration of devise completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'pages/home'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :books, only: [ :new, :create, :show, :edit, :update, :index, :destroy ]
   resources :watched_videos, only: [ :new, :create, :edit, :update, :index, :destroy ]
   resources :projects, only: [ :index, :new, :create, :edit, :update, :destroy ]
 
-  root 'pages#home'
+  root to: 'pages#home'
 end
