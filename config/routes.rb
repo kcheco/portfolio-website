66f0bd15bbf69a4921c809fdc7e81ad5766ed123
Kelvin Checo <checokelvin@gmail.com>
2019-01-25 01:12:17 -0500

    Added user model provided by devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 
   resources :books, only: [ :new, :create, :show, :edit, :update, :index, :destroy ]
   resources :watched_videos, only: [ :new, :create, :edit, :update, :index, :destroy ]
