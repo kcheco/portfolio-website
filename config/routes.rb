f933c064eed222f0fd4a850797ac5409efb40a95
Kelvin Checo <checokelvin@gmail.com>
2019-01-27 00:59:01 -0500

    Added sign in user feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, skip: [:sessions]
+  as :user do
+  #  get     'signup',       to: 'devise/registrations#new', as: 'new_user_registration_path'
+  #  post    'signup',       to: 'devise/registrations#create', as: ''
+   get     'admin/login',   to: 'devise/sessions#new',      as: 'new_user_session'
+   post    'admin/login',   to: 'devise/sessions#create',          as: 'user_session'
+   delete  'admin/logout',  to: 'devise/sessions#destroy',  as: 'destroy_user_session'
+  end
 
   resources :books, only: [ :new, :create, :show, :edit, :update, :index, :destroy ]
   resources :watched_videos, only: [ :new, :create, :edit, :update, :index, :destroy ]
