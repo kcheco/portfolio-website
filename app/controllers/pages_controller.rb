f17309eeecd9799c04ff664af01e489be711ae4a
Kelvin Checo <checokelvin@gmail.com>
2019-02-04 14:55:14 -0500

    Added feature to view my skills on the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
     @projects = Project.latest
     @books = Book.last_read
     @watched_videos = WatchedVideo.last_viewed
+    @skills = Skill.all
   end
 end
