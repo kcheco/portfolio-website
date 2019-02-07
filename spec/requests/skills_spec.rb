d65884c930640f7ee8b482fb045b74b08754222a
Kelvin Checo <checokelvin@gmail.com>
2019-02-06 22:43:30 -0500

    Restricted access to adding new skills when not signed in

--- spec/requests/skills_spec.rb
+++ spec/requests/skills_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'Skills', type: :request do
         get '/admin/skills'
       end
 
-      it 'returns success status code' do
+      it 'returns a success status' do
         expect(response.status).to eq 200
       end
     end
@@ -26,6 +26,31 @@ RSpec.describe 'Skills', type: :request do
       end
 
       it 'providers feedback to non-signed-in user' do
+        expect_not_authorized_flash_message
+      end
+    end
+  end
+
+  describe 'GET /admin/skills/new' do
+    context 'as a signed-in user' do
+      before do
+        sign_in @user
+        get '/admin/skills/new'
+      end
+
+      it 'returns a success status' do
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'as a non-signed-in user' do
+      before { get '/admin/skills/new' }
+
+      it 'redirects to home page' do
+        expect(response.status).to eq 302
+      end
+
+      it 'provides feedback to non-signed-in user' do
         expect_not_authorized_flash_message
       end
     end
